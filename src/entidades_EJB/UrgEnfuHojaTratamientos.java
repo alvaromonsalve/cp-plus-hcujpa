f8f21f223491d00b86d115dcd89edbe1b33f551c
Juan Camilo <Juan Camilo@Yehimison-PC>
2015-04-16 11:55:55 -0500

    impresion reportes enfermeria

---
+++ src/entidades_EJB/UrgEnfuHojaTratamientos.java
@@ -0,0 +1,211 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades_EJB;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+@Entity
+@Table(name = "enfu_hoja_tratamientos")
+@NamedQueries({
+    @NamedQuery(name = "UrgEnfuHojaTratamientos.findAll", query = "SELECT u FROM UrgEnfuHojaTratamientos u")})
+public class UrgEnfuHojaTratamientos implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "tipo")
+    private Integer tipo;
+    @Column(name = "identificador")
+    private Integer identificador;
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "hora")
+    @Temporal(TemporalType.TIME)
+    private Date hora;
+    @Column(name = "droga")
+    private String droga;
+    @Column(name = "dosis")
+    private String dosis;
+    @Column(name = "via")
+    private String via;
+    @Column(name = "observaciones")
+    private String observaciones;
+    @Column(name = "usr")
+    private Integer usr;
+    @Column(name = "estado")
+    private Character estado;
+    @Basic(optional = false)
+    @Column(name = "fecha_ingreso_datos")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaIngresoDatos;
+    @JoinColumn(name = "id_historia", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private InfoHistoriac idInfoHistoriac;
+
+    public InfoHistoriac getIdInfoHistoriac() {
+        return idInfoHistoriac;
+    }
+
+    public void setIdInfoHistoriac(InfoHistoriac idInfoHistoriac) {
+        this.idInfoHistoriac = idInfoHistoriac;
+    }
+
+    public UrgEnfuHojaTratamientos() {
+    }
+
+    public UrgEnfuHojaTratamientos(Integer id) {
+        this.id = id;
+    }
+
+    public UrgEnfuHojaTratamientos(Integer id, Date fechaIngresoDatos) {
+        this.id = id;
+        this.fechaIngresoDatos = fechaIngresoDatos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(Integer identificador) {
+        this.identificador = identificador;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Date getHora() {
+        return hora;
+    }
+
+    public void setHora(Date hora) {
+        this.hora = hora;
+    }
+
+    public String getDroga() {
+        return droga;
+    }
+
+    public void setDroga(String droga) {
+        this.droga = droga;
+    }
+
+    public String getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(String dosis) {
+        this.dosis = dosis;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public Integer getUsr() {
+        return usr;
+    }
+
+    public void setUsr(Integer usr) {
+        this.usr = usr;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaIngresoDatos() {
+        return fechaIngresoDatos;
+    }
+
+    public void setFechaIngresoDatos(Date fechaIngresoDatos) {
+        this.fechaIngresoDatos = fechaIngresoDatos;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UrgEnfuHojaTratamientos)) {
+            return false;
+        }
+        UrgEnfuHojaTratamientos other = (UrgEnfuHojaTratamientos) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades_EJB.UrgEnfuHojaTratamientos[ id=" + id + " ]";
+    }
+
+}
