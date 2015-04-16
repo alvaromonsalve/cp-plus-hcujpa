5eedbddd295f2f8a5d13cfff71bf2b61cf21e962
Juan Camilo <Juan Camilo@Yehimison-PC>
2015-04-16 17:20:55 -0500

    impresion reportes enfermeria

---
+++ src/entidades_EJB/HospAplicacionesOxigenoH.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades_EJB;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+@Entity
+@Table(name = "hosp_aplicaciones_oxigeno")
+@NamedQueries({
+    @NamedQuery(name = "HospAplicacionesOxigenoH.findAll", query = "SELECT h FROM HospAplicacionesOxigenoH h")})
+public class HospAplicacionesOxigenoH implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "hora_inicio")
+    private String horaInicio;
+    @Basic(optional = false)
+    @Column(name = "medico")
+    private int medico;
+    @Column(name = "sis_O2")
+    private Integer sisO2;
+    @Column(name = "fiO2")
+    private Integer fiO2;
+    @Column(name = "lts")
+    private Integer lts;
+    @Column(name = "usr")
+    private Integer usr;
+    @Column(name = "estado")
+    private Character estado;
+    @JoinColumn(name = "id_historiac", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private HospHistoriac idHospHistoriac;
+
+    public HospHistoriac getIdHospHistoriac() {
+        return idHospHistoriac;
+    }
+
+    public void setIdHospHistoriac(HospHistoriac idHospHistoriac) {
+        this.idHospHistoriac = idHospHistoriac;
+    }
+
+    public HospAplicacionesOxigenoH() {
+    }
+
+    public HospAplicacionesOxigenoH(Integer id) {
+        this.id = id;
+    }
+
+    public HospAplicacionesOxigenoH(Integer id, int medico) {
+        this.id = id;
+        this.medico = medico;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public int getMedico() {
+        return medico;
+    }
+
+    public void setMedico(int medico) {
+        this.medico = medico;
+    }
+
+    public Integer getSisO2() {
+        return sisO2;
+    }
+
+    public void setSisO2(Integer sisO2) {
+        this.sisO2 = sisO2;
+    }
+
+    public Integer getFiO2() {
+        return fiO2;
+    }
+
+    public void setFiO2(Integer fiO2) {
+        this.fiO2 = fiO2;
+    }
+
+    public Integer getLts() {
+        return lts;
+    }
+
+    public void setLts(Integer lts) {
+        this.lts = lts;
+    }
+
+    public Integer getUsr() {
+        return usr;
+    }
+
+    public void setUsr(Integer usr) {
+        this.usr = usr;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof HospAplicacionesOxigenoH)) {
+            return false;
+        }
+        HospAplicacionesOxigenoH other = (HospAplicacionesOxigenoH) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades_EJB.HospAplicacionesOxigenoH[ id=" + id + " ]";
+    }
+
+}
---
+++ src/entidades_EJB/UrgEnfuAplicacionesOxigeno.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades_EJB;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+@Entity
+@Table(name = "enfu_aplicaciones_oxigeno")
+@NamedQueries({
+    @NamedQuery(name = "UrgEnfuAplicacionesOxigeno.findAll", query = "SELECT u FROM UrgEnfuAplicacionesOxigeno u")})
+public class UrgEnfuAplicacionesOxigeno implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "hora_inicio")
+    private String horaInicio;
+    @Column(name = "sis_O2")
+    private Integer sisO2;
+    @Column(name = "fiO2")
+    private Integer fiO2;
+    @Column(name = "lts")
+    private Integer lts;
+    @Column(name = "usr")
+    private Integer usr;
+    @Column(name = "estado")
+    private Character estado;
+    @JoinColumn(name = "id_historiac", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private InfoHistoriac idInfoHistoriac;
+
+    public InfoHistoriac getIdInfoHistoriac() {
+        return idInfoHistoriac;
+    }
+
+    public void setIdInfoHistoriac(InfoHistoriac idInfoHistoriac) {
+        this.idInfoHistoriac = idInfoHistoriac;
+    }
+
+    public UrgEnfuAplicacionesOxigeno() {
+    }
+
+    public UrgEnfuAplicacionesOxigeno(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(String horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public Integer getSisO2() {
+        return sisO2;
+    }
+
+    public void setSisO2(Integer sisO2) {
+        this.sisO2 = sisO2;
+    }
+
+    public Integer getFiO2() {
+        return fiO2;
+    }
+
+    public void setFiO2(Integer fiO2) {
+        this.fiO2 = fiO2;
+    }
+
+    public Integer getLts() {
+        return lts;
+    }
+
+    public void setLts(Integer lts) {
+        this.lts = lts;
+    }
+
+    public Integer getUsr() {
+        return usr;
+    }
+
+    public void setUsr(Integer usr) {
+        this.usr = usr;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UrgEnfuAplicacionesOxigeno)) {
+            return false;
+        }
+        UrgEnfuAplicacionesOxigeno other = (UrgEnfuAplicacionesOxigeno) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades_EJB.UrgEnfuAplicacionesOxigeno[ id=" + id + " ]";
+    }
+    
+}
