d5dfdca9f989e8e8f16c0d4f7252b73a12c181c4
Alvaro Monsalve <alvaromonsalve@yahoo.com.co>
2015-07-02 15:09:32 -0500

    Entidades anexo 3 y ctc en Ingreso

--- src/entidades_EJB/UrgIngCtcProcedimientos.java
+++ src/entidades_EJB/UrgIngCtcProcedimientos.java
@@ -18,7 +18,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
+//lalala
 /**
  *
  * @author Juan Camilo
